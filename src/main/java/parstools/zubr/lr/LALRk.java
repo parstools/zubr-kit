d76c5e4cb1c4791d9fb72c24613fbd822dc81548
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-12-01 21:58:33 +0100

    fix: remove bad this.k = k;

--- src/main/java/parstools/zubr/lr/LALRk.java
+++ src/main/java/parstools/zubr/lr/LALRk.java
@@ -5,7 +5,6 @@ import parstools.zubr.grammar.Grammar;
 public class LALRk extends AbstractLR {
     int k;
     LALRk(Grammar g) {
-        this.k = k;
         StatesLRk states = new StatesLRk(g, k);
         states.createStates(this, null);
     }
